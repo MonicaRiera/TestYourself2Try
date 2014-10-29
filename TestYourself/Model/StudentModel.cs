f49eb0fc79dee119204849870f8866480aafca4f
MonicaRiera <monicarigi@hotmail.com>
2014-10-29 10:03:49 +0100

    TestYourself

---
+++ TestYourself/Model/StudentModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestYourself.Model
+{
+    class StudentModel
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _cpr;
+        private string _education;
+
+        public String FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public String LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Cpr
+        {
+            get { return _cpr; }
+            set { _cpr = value; }
+        }
+
+        public String Education
+        {
+            get { return _education; }
+            set { _education = value; }
+        }
+
+        public override string ToString()
+        {
+            return FirstName.ToString();
+        }
+
+    }
+}
---
+++ TestYourself/ViewModel/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TestYourself.Annotations;
+using TestYourself.Model;
+
+namespace TestYourself.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<StudentModel> _students;
+        private StudentModel _currentStudent;
+
+        public MainViewModel()
+        {
+            _students = new ObservableCollection<StudentModel>();
+
+            StudentModel s1 = new StudentModel();
+            s1.FirstName = "Monica";
+            s1.LastName = "Riera";
+            s1.Cpr = 123456;
+            s1.Education = "Computer Science";
+            _students.Add(s1);
+
+            StudentModel s2 = new StudentModel();
+            s2.FirstName = "Banu";
+            s2.LastName = "Tuncer";
+            s2.Cpr = 654321;
+            s2.Education = "Computer Science";
+            _students.Add(s2);
+
+            StudentModel s3 = new StudentModel();
+            s3.FirstName = "Kathleen";
+            s3.LastName = "Vam Ramselaar";
+            s3.Cpr = 112233;
+            s3.Education = "Marketing";
+            _students.Add(s3);
+        }
+
+        public ObservableCollection<StudentModel> Students
+        {
+            get { return _students; }
+            set { _students = value; }
+        }
+
+        public StudentModel CurrentStudent
+        {
+            get { return _currentStudent; }
+            set
+            {
+                _currentStudent = value;
+                OnPropertyChanged("CurrentStudent");
+            }
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
